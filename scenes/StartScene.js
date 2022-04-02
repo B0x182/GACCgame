9a86a079092abb31f23cd16511c611027092a59c
B0x182 <97475421+B0x182@users.noreply.github.com>
2022-04-02 02:47:36 +0200

    Update StartScene.js

--- scenes/StartScene.js
+++ scenes/StartScene.js
@@ -24,9 +24,67 @@ class StartScene extends Phaser.Scene {
 	   const { width, height } = this;    
        this.handlerScene.updateResize(this);
 	   
+	   // background image
+   		 this.add.image(0, 0, 'greens1').setOrigin(0).setScale(.75);
 	   
-	   var text = this.add.text(100,100, 'StartScene');
-
+	   
+	    var text = this.make.text({
+                x: this.width  / 2 ,
+                y: 50  ,
+                text: 'Select Game Mode',
+                style: {
+                    font: '20px monospace',
+                    fill: 'white'
+                }
+            });
+            text.setOrigin(.5);
+			text.setShadow(2, 2, "DarkSlateGray", 2);
+			
+	   
+	    var l1Text = this.make.text({
+                x: this.width  / 3 ,
+                y: this.height / 2 + 25 ,
+                text: 'Normal',
+                style: {
+                    font: '20px monospace',
+                    fill: 'white'
+                }
+            });
+            l1Text.setOrigin(.5);
+			l1Text.setShadow(2, 2, "DarkSlateGray", 2);
+		
+		
+		this.l1Btn = this.add.image(this.width  / 3 , this.height / 3, "level1")
+			.setOrigin(.5)
+			.setScale(.5)
+			.setDepth(1)
+			.setInteractive({ cursor: "pointer" })
+			.on('pointerdown',  () => this.startLevel1())
+		;
+        this.l1Btn.visible = true;
+		
+		
+		   var l2Text = this.make.text({
+                x: this.width  / 3 * 2,
+                y: this.height / 2 + 25 ,
+                text: 'Hard',
+                style: {
+                    font: '20px monospace',
+                    fill: 'white'
+                }
+            });
+            l2Text.setOrigin(.5);
+			l2Text.setShadow(2, 2, "DarkSlateGray", 2);
+		
+		
+		this.l2Btn = this.add.image(this.width  / 3 * 2 , this.height / 3, "level2")
+			.setOrigin(.5)
+			.setScale(.5)
+			.setDepth(1)
+			.setInteractive({ cursor: "pointer" })
+			.on('pointerdown',  () => this.startLevel2())
+		;
+        this.l2Btn.visible = true;
 		
 
 	}
@@ -36,6 +94,20 @@ class StartScene extends Phaser.Scene {
 	}
 
 
+	startLevel1()
+	{
+		this.sceneStopped = true;
+        this.scene.stop('StartScene');
+        this.handlerScene.launchScene('GameScene', 1);
+		
+	}
+	
+	startLevel2()
+	{
+		this.sceneStopped = true;
+        this.scene.stop('StartScene');
+        this.handlerScene.launchScene('GameScene', 2);
+	}
 
 }
 
